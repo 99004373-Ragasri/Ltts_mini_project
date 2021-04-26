5599cd7484851a37406081f4e2eba3a18b1d9ff8
ragasrikonakalla <39005938+ragasrikonakalla@users.noreply.github.com>
2021-04-26 22:01:57 +0530

    Update hangman.c

--- 3_implementation/hangman.c
+++ 3_implementation/hangman.c
@@ -10,15 +10,12 @@
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
+#include "hang.h"
 
 char word[50];
-void clrscr(void);
-char (*loadDict(int *n))[50];
-int getRandom(int n);
-void displayDictionary(char [][50], int n);
-bool checkAlphabet(char *, char);
 
 int main() {
+    void hang.loop1();
     printf("~~~~~~~~~~~~~~~:HANGMAN:~~~~~~~~~~~~~~~\n~~~~~~~~~~~Word guess game~~~~~~~~~~~\n");
     int n;
     char (*dict)[50] = loadDict(&n);
@@ -94,47 +91,4 @@ int main() {
 }
    
 
-void displayDictionary(char dict[][50], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", dict[i]);
-    }
-}
 
-int getRandom(int n) {
-    static bool flag = true;
-    if (flag) {
-        srand(time(NULL));
-        flag = false;
-    }
-    return rand() % n;
-}
-
-void clrscr(void) {
-    printf("\033[2J");
-    printf("\033[0;0f");
-}
-
-char (*loadDict(int *n))[50] {
-    
-     long wordsCount = 20;
-    FILE *fp =  fopen("verySmallDictionary.txt", "r");
-
-    // DO NOT DISTURB BELOW CODE.
-    if (!fp) {
-        *n = -1;
-        return NULL;
-    }
-    char (*dict)[50] = (char(*)[50])malloc(wordsCount * 50);
-    if (!dict) {
-        *n = -2;
-        return NULL;
-    }
-    int i = 0;
-    while (fgets(dict[i], 50, fp) != NULL) {
-        dict[i][strlen(dict[i]) - 1] = '\0';
-        i++;
-    }
-    fclose(fp);
-    *n = i;
-    return dict;
-}
