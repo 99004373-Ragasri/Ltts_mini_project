c1df1e8874712951a293d67031d9341341e8420f
ragasrikonakalla <39005938+ragasrikonakalla@users.noreply.github.com>
2021-04-27 22:01:09 +0530

    Update test_hang.c

--- 3_implementation/test/test_hang.c
+++ 3_implementation/test/test_hang.c
@@ -1,8 +1,6 @@
 #include "hang.h"
 #include "unity.h"
 
-
-char test_(*loadDict(int *n))[50];
 int test_getRandom(int n);
 
 void setUp(){
@@ -17,7 +15,6 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_(*loadDict(int *n)));
   RUN_TEST(test_getRandom);
  
 
@@ -25,12 +22,6 @@ int main()
   return UNITY_END();
 }
 
-char test_(*loadDict(int *n))[50]{
-  n=4;
-  TEST_ASSERT_EQUAL((dict, *loadDict(4,dict));
-  
-
-}
 int test_getRandom(int n){
 n=4
 flag = true
